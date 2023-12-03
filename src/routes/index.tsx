78f62f44938eed6c1056ddded459d0f82f128611
PolyDevil <polydevil@cocaine.ninja>
2023-12-03 14:00:07 +0300

    Update index.tsx

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -114,11 +114,5 @@ export default component$(() => {
 });
 
 export const head: DocumentHead = {
-  title: "Welcome to Qwik",
-  meta: [
-    {
-      name: "description",
-      content: "Qwik site description",
-    },
-  ],
+  title: "Bankimonline Тестовое задание",
 };
